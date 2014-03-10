a397a526d617787ee236d0fb3130b26f11ad7234
Fernando Jiménez <ferjmoreno@gmail.com>
2014-03-10 16:53:09 +0100

    Use process.env instead of tokbox_credentials.json

--- tokbox/opentok.js
+++ tokbox/opentok.js
@@ -1,22 +1,20 @@
-var OpenTok = require('opentok'),
-    fs = require('fs');
+var OpenTok = require('opentok');
 
 // Get API key and secret from local filesystem.
 // For now we will only manage a single API key, but we might need to
 // handle multiple API keys soon.
 // We expose the API key so clients won't need to do any key selection logic
 // if this is needed at some point.
-var data = fs.readFileSync(__dirname + '/tokbox_credentials.json');
-try {
-  credentials = JSON.parse(data);
-}
-catch (err) {
-  console.log('There has been an error parsing Tokbox credentials');
-  console.log(err);
-  return;
+
+var apiKey = process.env.TB_KEY;
+var apiSecret = process.env.TB_SECRET;
+
+if (!apiKey || !apiSecret) {
+  console.error('ERROR: You need to set TB_KEY and TB_SECRET');
+  process.exit(1);
 }
 
-var opentok = new OpenTok.OpenTokSDK(credentials.key, credentials.secret);
-opentok.apiKey = credentials.key;
+var opentok = new OpenTok.OpenTokSDK(apiKey, apiSecret);
+opentok.apiKey = apiKey;
 
 module.exports = opentok;
